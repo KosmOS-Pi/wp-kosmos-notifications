ece6286935b789cccdc71dc1a6228828be39e3b1
Matteo Piscitelli <matteo@matteopiscitelli.it>
2025-12-12 15:10:18 +0100

    Filter post by validity date. Caching by ETag. Ver. 1.1.
      * Filter out posts outside validity date on server side.
      * Caching is based on ETag, modified time is provided but
        not used to determine output 200/304.
      * Added license header.

--- kosmos-notifications.php
+++ kosmos-notifications.php
@@ -1,9 +1,23 @@
 <?php
 /*
-Plugin Name: Kosmos Notifications REST Helper
+Plugin Name: KosmOS Notifications REST Helper
 Description: Exposes notification posts via REST API at /wp-json/kosmos/v1/notifications
-Version: 1.0
-Author: Kosmos Project
+Version: 1.1
+
+Copyright: Matteo Piscitelli 2025
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
 if (!defined('ABSPATH')) exit;
@@ -34,25 +48,77 @@ add_action('rest_api_init', function() {
             $cat      = get_category_by_slug($cat_slug);
             $cat_id   = $cat ? $cat->term_id : 0;
 
+            //compute query
+            $today = current_time('Y-m-d');
+            $meta_query = [
+                'relation' => 'AND',
+
+                // always: notify_users = 1
+                [
+                    'key'     => 'notify_users',
+                    'value'   => 1,
+                    'compare' => '=',
+                    'type'    => 'NUMERIC'
+                ],
+
+                // START DATE: missing or <= today
+                [
+                    'relation' => 'OR',
+                    [
+                        'key'     => 'start_date',
+                        'compare' => 'NOT EXISTS',
+                    ],
+                    [
+                        'key'     => 'start_date',
+                        'value'   => $today,
+                        'compare' => '<=',
+                        'type'    => 'DATE',
+                    ],
+                    [
+                        'key'     => 'start_date',
+                        'value'   => '',
+                        'compare' => '=',
+                    ],
+                ],
+
+                // END DATE: empty or >= today
+                [
+                    'relation' => 'OR',
+                    [
+                        'key'     => 'end_date',
+                        'compare' => 'NOT EXISTS',
+                    ],
+                    [
+                        'key'     => 'end_date',
+                        'value'   => $today,
+                        'compare' => '>=',
+                        'type'    => 'DATE',
+                    ],
+                    [
+                        'key'     => 'end_date',
+                        'value'   => '',
+                        'compare' => '=',
+                    ],
+                ],
+            ];
+
             // Query posts
             $args = [
                 'post_type'      => 'post',
                 'post_status'    => 'publish',
                 'posts_per_page' => 10,
                 'category__in'   => $cat_id ? [$cat_id] : [],
-                'meta_query'     => [
-                    [
-                        'key'     => 'notify_users',
-                        'value'   => '1',
-                        'compare' => '=',
-                    ],
-                ],
+                'meta_query'     => $meta_query,
                 'orderby' => 'date',
                 'order'   => 'DESC',
             ];
 
+
+
             $q = new WP_Query($args);
             $items = [];
+
+            $last_mod = 0;
 
             foreach ($q->posts as $post) {
                 $id = $post->ID;
@@ -85,19 +151,24 @@ add_action('rest_api_init', function() {
                     'priority'   => $priority,
                     'date'       => get_the_date('c', $id),
                 ];
-            }
 
-            // Compute last modified time for caching
-            $last_mod = 0;
-            foreach ($q->posts as $p) {
-                $t = strtotime($p->post_modified_gmt . ' GMT');
+                $t = strtotime($post->post_modified_gmt . ' GMT');
                 if ($t > $last_mod) $last_mod = $t;
             }
+
+            $etag = '"' . md5(json_encode($items)) . '"';
+            $client_etag = $request->get_header('if-none-match');
+
+            if ($client_etag && $client_etag === $etag) {
+                return new WP_REST_Response(null, 304);
+            }
+
             if (!$last_mod) $last_mod = time();
 
             // Build REST response
             $response = rest_ensure_response($items);
             $response->header('Content-Type', 'application/json; charset=utf-8');
+            $response->header('ETag', $etag);
             $response->header('Last-Modified', gmdate('D, d M Y H:i:s', $last_mod) . ' GMT');
             return $response;
         },
